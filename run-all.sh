21fb71adb0954eaef8c995da3a5991be5d021c1c
João Lopes <lopesjoaop3@gmail.com>
2023-06-06 20:27:22 +0100

    make script restart network on every run

--- run-all.sh
+++ run-all.sh
@@ -3,18 +3,20 @@
 NETWORK_ENV_FILE='./network/.env'
 TIMESTAMP=$(date +%s)
 
-mkdir -p results
+RESULTS_DIR=./results/"$TIMESTAMP"
+
+mkdir -p "$RESULTS_DIR"
 
 for NODES_NUMBER in 7 14 21 28; do
   sed -i 's/NODES_NUMBER=.*/NODES_NUMBER='$NODES_NUMBER'/' $NETWORK_ENV_FILE
-  for CONSENSUS_ALGO in qbft ibft raft clique; do
+  for CONSENSUS_ALGO in raft clique ibft qbft; do
     sed -i 's/GOQUORUM_CONS_ALGO=.*/GOQUORUM_CONS_ALGO='$CONSENSUS_ALGO'/' $NETWORK_ENV_FILE
-    cd ./network || exit 1
-    ./run.sh
-    sleep 5 # wait for network to init
-    cd ..
     for TYPE in public private; do
       for TPS in 50 100 150 200 250 300; do
+        cd ./network || exit 1
+        ./run.sh
+        cd ..
+        sleep 120 # wait for network to init
         sed -i 's/tps:.*/tps: '$TPS'/' ./caliper/benchmarks/scenario/bank-"$TYPE"/config.yaml
         echo "Executing benchmark with $NODES_NUMBER, $CONSENSUS_ALGO, $TPS, $TYPE"
 
@@ -26,13 +28,12 @@ for NODES_NUMBER in 7 14 21 28; do
         fi
 
         cd ..
-        cp ./caliper/report.html ./results/report-$NODES_NUMBER-$CONSENSUS_ALGO-$TYPE-$TPS-"$TIMESTAMP".html
+        cp ./caliper/report.html "$RESULTS_DIR"/report-$CONSENSUS_ALGO-$NODES_NUMBER-$TYPE-$TPS.html
+        cd ./network || exit 1
+        ./remove.sh
+        cd ..
         echo "Benchmark ran successfully"
-        sleep 5 # ensure network is not processing previous test
       done
     done
-    cd ./network || exit 1
-    ./remove.sh
-    cd ..
   done
 done
